f5b945eb27db007e43b54676556efdf5e46e06a0
Abrar Na'im <70412104+abrarnaim015@users.noreply.github.com>
2020-09-27 00:49:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hallo-Indonesia-Ku
---
+++ js.js
@@ -0,0 +1,76 @@
+function jwbluffy()
+{
+    let luffyB = document.getElementsByClassName("luffyT")
+    luffyB[0].classList.remove("display-none")
+}
+
+function jwbzoro()
+{
+    let zoroB = document.getElementsByClassName("zoroT")
+    zoroB[0].classList.remove("display-none")
+}
+
+function jwbnami() 
+{
+    let namiB = document.getElementsByClassName("namiT");
+    namiB[0].classList.remove("display-none");
+    
+}
+
+function jwbusopp() 
+{
+    let usoppB = document.getElementsByClassName("usoppT");
+    usoppB[0].classList.remove("display-none");
+}
+
+function jwbsanji() 
+{
+    let sanjiB = document.getElementsByClassName("sanjiT");
+    sanjiB[0].classList.remove("display-none");
+}
+
+function jwbchopper() 
+{
+    let chopperB = document.getElementsByClassName("chopperT");
+    chopperB[0].classList.remove("display-none");
+}
+
+function jwbrobin() 
+{
+    let robinB = document.getElementsByClassName("robinT");
+    robinB[0].classList.remove("display-none");
+}
+
+function jwbfranky() 
+{
+    let frankyB = document.getElementsByClassName("frankyT");
+    frankyB[0].classList.remove("display-none");
+}
+
+function jwbbrook() 
+{
+    let brookB = document.getElementsByClassName("brookT");
+    brookB[0].classList.remove("display-none");
+}
+
+function bonus1() 
+{
+    let b1 = document.getElementsByClassName("imgBonus1");
+    b1[0].classList.remove("display-none");
+}
+
+function bonus3() 
+{
+    let b3 = document.getElementsByClassName("imgBonus3");
+    b3[0].classList.remove("display-none");
+    b3[1].classList.remove("display-none");
+    b3[2].classList.remove("display-none");
+}
+
+function bonus7() 
+{
+    let b7 = document.getElementsByClassName("imgBonus7");
+    b7[0].classList.remove("display-none");
+    b7[1].classList.remove("display-none");
+    b7[2].classList.remove("display-none");
+}
